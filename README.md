c8a7c1b943f0a003eae285a57949cb9a3c00c9e7
stream <penolove15@gmail.com>
2017-02-16 18:26:04 +0800

    add Checksum check

--- README.md
+++ README.md
@@ -1,24 +1,35 @@
 # FDDB_DataSet_4_faster_rcnn
 
-# get datas from FDDB
+## Step1: get datas from FDDB
 ```
 ./get_data.sh
 ```
 this should downloads originalPics.tar.gz(~500MB), and FDDB-folds.tgz from FDDB
-and unzip tar.gz into originalPics directory.
+and Checksum test ,if pass -> unzip tar.gz  into originalPics directory.
 
-#create data set that can be used in pyfaster-rcnn
+if link fails : download from  FDDB website 
+http://vis-www.cs.umass.edu/fddb/
+
+
+if always checksum fails :
+
+```
+wget http://tamaraberg.com/faceDataset/originalPics.tar.gz 
+wget http://vis-www.cs.umass.edu/fddb/FDDB-folds.tgz
+
+mkdir originalPics;
+tar -C originalPics  -zxf originalPics.tar.gz
+tar -C originalPics  -zxf FDDB-folds.tgz
+```
+
+
+## Step2: create data set that can be used in pyfaster-rcnn
 ```
 cd pyxml;
 ./runit.sh;
 ```
 this will create FDDB_2010 directory
 and JPEGImages/Annotation directory inside FDDB_2010
-
-you can use labelImg to see if it deals properly(This is god dame awesome)
-https://github.com/tzutalin/labelImg 
-![alt tag](https://raw.githubusercontent.com/penolove/FDDB_DataSet_4_faster_rcnn/master/FDDB.png)
-
 
 runit.sh contains:
 ```
@@ -29,4 +40,11 @@ python anno2xml.py FDDB-fold-03-ellipseList.txt;
 ```
 which FDDB-fold-01-ellipseList.txt were in the originalPics/FDDB-folds
 you can check if the list are the same (01~10)
+
+
+## Step3: using labelImg to test if create properly
+you can use labelImg to see if it deals properly(This is god dame awesome)
+https://github.com/tzutalin/labelImg 
+![alt tag](https://raw.githubusercontent.com/penolove/FDDB_DataSet_4_faster_rcnn/master/FDDB.png)
+
 
--- get_data.sh
+++ get_data.sh
@@ -1,11 +1,48 @@
-wget http://tamaraberg.com/faceDataset/originalPics.tar.gz
+ori_CheckSum=cf414253ac596cd858daae0cc321d793
+folds_CheckSum=4cf9badc939a3398a0d6f3a3c8540f55
 if [ -d originalPics ];
 then
     echo "dir alreday exist";
 else
+    # ---- download originalPics.tar.gz ----
+    FILE=originalPics.tar.gz
+    #if file not exist
+    if [ ! -f $FILE ]; then
+        echo "Downloading originalPics.tar.gz ....."
+        wget http://tamaraberg.com/faceDataset/originalPics.tar.gz 
+    fi
+
+    checksum=`md5sum $FILE | awk '{ print $1 }'`
+    if [ ! "$checksum" = "$ori_CheckSum" ]; then 
+        rm $File
+        echo $checksum
+        echo $folds_CheckSum
+        echo file $FILE : checksum error , need to rerun the script;
+        exit 1;
+    fi
+
+    # ---------------------------------------
+
+    # ---- download FDDB.tgz ----
+    FILE=FDDB-folds.tgz
+    #if file not exist
+    if [ ! -f $FILE ]; then
+        echo "Downloading FDDB-folds.tgz ....."
+        wget http://vis-www.cs.umass.edu/fddb/FDDB-folds.tgz
+    fi
+
+    checksum=`md5sum $FILE | awk '{ print $1 }'`
+    if [ ! "$checksum" = "$folds_CheckSum" ]; then 
+        rm $FILE
+        echo $checksum
+        echo $folds_CheckSum
+        echo file $FILE : checksum error , need to rerun the script;
+        exit 1;
+    fi
+
+    echo "Making originalPics , uncompress files ..."
     mkdir originalPics;
+    tar -C originalPics  -zxf originalPics.tar.gz
+    tar -C originalPics  -zxf FDDB-folds.tgz
+
 fi;
-tar -C originalPics  -zxf originalPics.tar.gz
-wget http://vis-www.cs.umass.edu/fddb/FDDB-folds.tgz
-tar -C originalPics  -zxf FDDB-folds.tgz
-rm FDDB-folds.tgz originalPics.tar.gz
